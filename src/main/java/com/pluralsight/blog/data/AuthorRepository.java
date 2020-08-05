e370609e82d1c95c8e88d291e45168ad942e21c9
Karsten Spakowski <kspakowski@web.de>
2020-08-05 14:16:27 +0200

    module2

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -4,18 +4,13 @@ import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class AuthorRepository {
-    public List<Author> findAll() {
-        return new ArrayList<>();
-    }
+@RepositoryRestResource(exported = false)
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    public List<Author> saveAll(List<Author> authors) {
-        return new ArrayList<>();
-    }
 }
